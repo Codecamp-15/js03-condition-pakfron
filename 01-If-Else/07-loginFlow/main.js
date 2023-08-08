923dec85f65ab79be34813f03e9f0849716b74b5
pakfron <pakpoom.poom@hotmail.com>
2023-08-08 09:01:15 +0700

    edit 01-07-loginflow

--- 01-If-Else/07-loginFlow/main.js
+++ 01-If-Else/07-loginFlow/main.js
@@ -1,18 +1,18 @@
 let uname = prompt("Username")
 let pword = prompt("Password")
 
-if (uname==""||pword==String)
-{
+ if (uname == "" && pword == "") {
+    alert("user and password is required")
+}
+else if (uname == "" || pword == String) {
     alert("username is required")
 }
-else if (uname==String||pword=="")
-{
+else if (uname == String || pword == "") {
     alert("password is required")
 }
 
-else if (uname=="admin"&&pword=="1234"||uname=="john"&&pword=="qwerty")
-{
-    alert("Hello" +" " +uname)
+else if (uname == "admin" && pword == "1234" || uname == "john" && pword == "qwerty") {
+    alert("Hello" + " " + uname)
 }
 else {
     alert("invalid username or password")
